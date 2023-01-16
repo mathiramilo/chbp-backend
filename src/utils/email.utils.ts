c17f9aaf5f91099ca531c689aa73e57cf2793dc2
Mathias Ramilo <mathiramilo2290@gmail.com>
2023-01-16 18:14:09 -0300

    Comments deleted

--- src/utils/email.utils.ts
+++ src/utils/email.utils.ts
@@ -27,27 +27,3 @@ const sendEmail = async options => {
 }
 
 export default sendEmail
-
-// export const sendRegisterMail = async newUser => {
-//   const bodyHtml = `
-//     <div>
-//       <p>Email: ${newUser.email}</p>
-//       <p>Name: ${newUser.name}</p>
-//       <p>Address: ${newUser.address}</p>
-//       <p>Age: ${newUser.age}</p>
-//       <p>Phone Number: ${newUser.phone}</p>
-//     </div>
-//   `
-
-//   const mailOptions = {
-//     from: '"CHBP Server" orrin89@ethereal.email',
-//     to: envConfig.ADMIN_EMAIL,
-//     subject: `New user registered`,
-//     html: bodyHtml
-//   }
-//   const info = await transporter.sendMail(mailOptions)
-
-//   logger.info(
-//     `Email sended! Preview URL: ${nodemailer.getTestMessageUrl(info)}`
-//   )
-// }
