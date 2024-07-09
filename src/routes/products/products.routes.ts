7ab3abd6ada4b1d459c0ffb0b7c4bfe53dd7c69a
Mathias Ramilo <mathiramilo2290@gmail.com>
2024-07-09 05:42:46 -0300

    Fix: remove auth middleware for GET products and product details

--- src/routes/products/products.routes.ts
+++ src/routes/products/products.routes.ts
@@ -5,12 +5,10 @@ import authMiddleware from '../../middlewares/auth.middleware'
 
 const router = Router()
 
-router.use(authMiddleware)
-
 router.get('/', ProductsController.getProducts)
 router.get('/:id', ProductsController.getProductById)
-router.post('/', adminMiddleware, ProductsController.saveProduct)
-router.put('/:id', adminMiddleware, ProductsController.updateProduct)
-router.delete('/:id', adminMiddleware, ProductsController.deleteProduct)
+router.post('/', [authMiddleware, adminMiddleware], ProductsController.saveProduct)
+router.put('/:id', [authMiddleware, adminMiddleware], ProductsController.updateProduct)
+router.delete('/:id', [authMiddleware, adminMiddleware], ProductsController.deleteProduct)
 
 export default router
